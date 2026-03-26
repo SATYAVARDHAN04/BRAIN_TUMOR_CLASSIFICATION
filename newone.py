cef78f7cbb992e03b9554099bfb99182eff4bb7e
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-03-27 00:00:30 +0530

    CAPSTONE

--- newone.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print(f"✅ GPU Found: {torch.cuda.get_device_name(0)}")
-    print(f"Total VRAM: {torch.cuda.get_device_properties(0).total_memory / 1024**3:.2f} GB")
-else:
-    print("❌ No NVIDIA GPU found (or CUDA is not installed).")
